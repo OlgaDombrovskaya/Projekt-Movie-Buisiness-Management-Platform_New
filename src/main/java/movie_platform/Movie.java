4cc9cea8f41c243a525bf82af4cbb6cf8d4c7b92
Olga Dombrovskaya <olgaekb26@gmail.com>
2025-02-22 03:21:04 +0000

    changes in class Movie and MovieManager

--- src/main/java/movie_platform/Movie.java
+++ src/main/java/movie_platform/Movie.java
@@ -1,18 +1,28 @@
 package movie_platform;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
 import java.util.List;
-
+@Slf4j
 public class Movie {
-    private String id;
-    private String title;
-    private String genre;
-    private String startDate;
-    private String endDate;
-    private MovieStatus status;
-    private double budget;
-    private List<String> producer;
-    private List<String> actors;
+    private String id;                        // Уникальный идентификатор фильма
+    private String title;                     // Название фильма
+    private String genre;                     // Жанр фильма
+    private String startDate;                 // Дата начала показа фильма
+    private String endDate;                   // Дата окончания показа фильма
+    private MovieStatus status;               // Статус фильма (PLANNED, IN_PROGRESS, COMPLETED)
+    private double budget;                    // Бюджет фильма
+    private List<String> producer;            // Список продюсеров фильма
+    private List<String> actors;              // Список актёров фильма
 
+    // Конструктор с минимальным набором данных для создания фильма
+    public Movie(String movieId, String title, MovieStatus movieStatus) {
+        this.id = movieId;
+        this.title = title;
+        this.status = movieStatus;
+    }
+    // Конструктор, инициализирующий все поля фильма
     public Movie(String id, String title, String genre, String startDate, String endDate,
                  MovieStatus status, double budget, List<String> producer, List<String> actors) {
 
@@ -27,6 +37,7 @@ public class Movie {
         this.actors = actors;
     }
 
+    // Геттеры для получения значений полей
     public String getId() {
         return id;
     }
@@ -56,13 +67,20 @@ public class Movie {
     }
 
     public List<String> getProducer() {
+        if (producer == null) {
+            return new ArrayList<>();
+        }
         return producer;
     }
 
     public List<String> getActors() {
+        if (actors == null) {
+            return new ArrayList<>();
+        }
         return actors;
     }
 
+    // Сеттеры с проверками входных данных
     public void setId(String id) {
         if (id == null || id.trim().isEmpty()) {
             System.out.println("ID не может быть пустым.");
@@ -88,10 +106,18 @@ public class Movie {
     }
 
     public void setStartDate(String startDate) {
+        if (startDate == null || startDate.trim().isEmpty()) {
+            System.out.println("Дата начала не может быть пустой.");
+            log.warn("Дата начала не может быть пустой.");
+        }
         this.startDate = startDate;
     }
 
     public void setEndDate(String endDate) {
+        if (endDate == null || endDate.trim().isEmpty()) {
+            System.out.println("Дата окончания не может быть пустой.");
+            log.warn("Дата окончания не может быть пустой.");
+        }
         this.endDate = endDate;
     }
 
@@ -104,8 +130,8 @@ public class Movie {
     }
 
     public void setBudget(double budget) {
-        if (budget < 0) {
-            System.out.println("Бюджет не может быть отрицательным.");
+        if (budget <= 0) {
+            System.out.println("Бюджет не может быть отрицательным или меньше 0.");
             return;
         }
         this.budget = budget;
@@ -127,16 +153,20 @@ public class Movie {
         this.actors = actors;
     }
 
+    // Методы для работы с продюсерами
     public void addProducer(String producerName) {
         if (producerName == null || producerName.trim().isEmpty()) {
             System.out.println("Имя продюсера не может быть пустым.");
+            log.warn("Имя продюсера не может быть пустым.");
             return;
         }
         if (!producer.contains(producerName)) {
             producer.add(producerName);
-            System.out.println("Режиссёр добавлен: " + producerName);
+            System.out.println("Продюсера добавлен: " + producerName);
+            log.info("Продюсера добавлен: {}", producerName);
         } else {
-            System.out.println("Этот режиссёр уже добавлен.");
+            System.out.println("Этот продюсера уже добавлен.");
+            log.warn("Этот продюсера уже добавлен.");
         }
     }
 
@@ -144,26 +174,33 @@ public class Movie {
 
         if (producerName == null || producerName.trim().isEmpty()) {
             System.out.println("Имя продюсера не может быть пустым.");
+            log.warn("Имя продюсера не может быть пустым.");
             return;
         }
         if (producer.remove(producerName)) {
-            System.out.println("Режиссёр удалён: " + producerName);
+            System.out.println("Продюсера удалён: " + producerName);
+            log.info("Продюсера удалён: {}", producerName);
         } else {
-            System.out.println("Режиссёр не найден.");
+            System.out.println("Продюсера не найден.");
+            log.info("Продюсера не найден.");
         }
     }
 
+    // Методы для работы с актёрами
     public void addActor(String actorName) {
 
         if (actorName == null || actorName.trim().isEmpty()) {
             System.out.println("Имя актёра не может быть пустым.");
+            log.warn("Имя актёра не может быть пустым.");
             return;
         }
         if (!actors.contains(actorName)) {
             actors.add(actorName);
             System.out.println("Актёр добавлен: " + actorName);
+            log.info("Актёр добавлен: {}", actorName);
         } else {
             System.out.println("Этот актёр уже добавлен.");
+            log.info("Этот актёр уже добавлен.");
         }
     }
 
@@ -171,41 +208,67 @@ public class Movie {
 
         if (actorName == null || actorName.trim().isEmpty()) {
             System.out.println("Имя актёра не может быть пустым.");
+            log.warn("Имя актёра не может быть пустым.");
             return;
         }
         if (actors.remove(actorName)) {
             System.out.println("Актёр удалён: " + actorName);
+            log.info("Актёр удалён: {}", actorName);
         } else {
             System.out.println("Актёр не найден.");
+            log.info("Актёр не найден.");
         }
     }
 
     public void updateStatus(MovieStatus newStatus) {
         if (newStatus == null) {
             System.out.println("Некорректный статус. Обновление не выполнено.");
+            log.warn("Некорректный статус. Обновление не выполнено.");
             return;
         }
-        MovieStatus status = MovieStatus.values()[0];
 
+        // Метод для обновления статуса фильма
         switch (newStatus) {
             case PLANNED:
                 System.out.println("Фильм теперь находится в производстве.");
+                log.info("Фильм теперь находится в производстве.");
                 break;
             case IN_PROGRESS:
                 System.out.println("Фильм теперь выпущен.");
+                log.info("Фильм теперь выпущен.");
                 break;
             case COMPLETED:
                 System.out.println("Фильм отменён.");
+                log.info("Фильм отменён.");
                 break;
             default:
                 System.out.println("Неизвестный статус.");
+                log.warn("Неизвестный статус.");
         }
 
         if (status == newStatus) {
             System.out.println("Фильм уже имеет статус: " + newStatus);
+            log.info("Фильм уже имеет статус: {}", newStatus);
             return;
         }
+        this.status = newStatus;
         System.out.println("Статус фильма успешно обновлён на: " + newStatus);
+        log.info("Статус фильма успешно обновлён на: {}", newStatus);
+    }
 
+    // Переопределение метода toString() для вывода информации о фильме
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", genre='" + genre + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", status=" + status +
+                ", budget=" + budget +
+                ", producer=" + producer +
+                ", actors=" + actors +
+                '}';
     }
 }
